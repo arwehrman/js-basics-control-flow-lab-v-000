0ecce4889f437fab72521ce067cabe49209fd2b5
Amanda Wehrman <arwehrman@gmail.com>
2018-01-07 18:58:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
   return 'This one is on me!'
 }
-  else if (distance > 2000) {
+else if (distance > 2500){
+  return 'No can do.'
+}
+  else (distance > 2000) {
   return 'I will gladly take your thirty bucks.'
   }
-  else (distance > 2500){
-    return 'No can do.'
 
-  }
 }
