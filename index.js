e8eb8031bf3e7610ea4df942f0cffa002fc524a5
Amanda Wehrman <arwehrman@gmail.com>
2018-01-07 19:00:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(distance) {
-  if (distance > 2000) {
-  return 'I will gladly take your thirty bucks.'
+  if (distance > 2500){
+    return 'No can do.'
 }
-else if (distance > 2500){
-  return 'No can do.'
+else if (distance > 2000) {
+return 'I will gladly take your thirty bucks.'
 }
   else (distance <= 400) {
   return 'This one is on me!'
